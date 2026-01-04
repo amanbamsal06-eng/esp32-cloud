3fd238d21d946b15a045c817ec9ca56b24675440
amanbamsal06-eng <amanbamsal06@gmail.com>
2026-01-04 13:49:35 +0000

    api ipdate

--- main.py
+++ main.py
@@ -2,15 +2,31 @@ from fastapi import FastAPI, Header, HTTPException
 
 app = FastAPI()
 
+# temporary in-memory store
 DEVICE_TOKEN = "DEV_TEST_123"
+device_state = "OFF"
 
 @app.get("/")
 def root():
     return {"status": "server running"}
 
+# App / user sends command
+@app.post("/send_command")
+def send_command(command: str, authorization: str = Header(None)):
+    global device_state
+    if authorization != DEVICE_TOKEN:
+        raise HTTPException(status_code=401)
+
+    if command not in ["ON", "OFF"]:
+        raise HTTPException(status_code=400)
+
+    device_state = command
+    return {"status": "stored", "command": device_state}
+
+# ESP32 polls this
 @app.get("/device/command")
 def get_command(authorization: str = Header(None)):
     if authorization != DEVICE_TOKEN:
         raise HTTPException(status_code=401)
-    return {"command": "OFF"}
-    
+
+    return {"command": device_state}
