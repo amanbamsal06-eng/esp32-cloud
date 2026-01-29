45654de7351649062895ab912fcef2fe5b1d7ca8
amanbamsal06-eng <amanbamsal06@gmail.com>
2026-01-29 23:00:52 +0530

    test

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ if not SUPABASE_URL or not SUPABASE_KEY:
 if not SUPABASE_URL or not SUPABASE_KEY:
     raise RuntimeError("Supabase env vars not set")
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # -------------------------------------------------
 # MQTT CLIENT
